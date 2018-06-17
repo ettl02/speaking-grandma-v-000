affdbfc3fddbc878fa471114e43959dd9f54e493
Emily Wessel <ettl02@gmail.com>
2018-06-17 18:14:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,8 @@ elsif phrase == phrase.downcase
   return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.downcase
+      return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
 else phrase == phrase.upcase
