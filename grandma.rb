33cb63996f36cb6e316e62c1c722893631e4aeea
Emily Wessel <ettl02@gmail.com>
2018-06-17 16:53:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,10 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma (phrase)
+  if phrase == uppercase
+    return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!" 
+    "
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
