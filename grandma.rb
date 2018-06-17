6bf020adf15908e37320a865c837c951152fd6df
Emily Wessel <ettl02@gmail.com>
2018-06-17 17:21:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,8 @@ def speak_to_grandma (phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif
     return "NO, NOT SINCE 1938!"
-  else 
-    phrase == "I LOVE YOU GRANDMA!"
+  else
     return "I LOVE YOU TOO PUMPKIN!"
-
 end
 end
 
