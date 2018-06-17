5c04ac1c6cea989387b1e6c729defef911234790
Emily Wessel <ettl02@gmail.com>
2018-06-17 18:06:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,10 @@ if phrase == "I LOVE YOU GRANDMA"
   puts "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.downcase
   puts "HUH?! SPEAK UP, SONNY!"
+elsif phrase == phrase.downcase 
+    puts "HUH?! SPEAK UP, SONNY!"
+elsif phrase == phrase.upcase
+  puts "NO, NOT SINCE 1938!"
 else phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
 end
