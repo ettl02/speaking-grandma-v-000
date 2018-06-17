e7c7a3a89ef1d1aa4c2eafd38ec3a6e2d313e479
Emily Wessel <ettl02@gmail.com>
2018-06-17 18:04:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA"
   puts "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == phrase.downcase 
+elsif phrase == phrase.downcase
   puts "HUH?! SPEAK UP, SONNY!"
 else phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
-end 
-end 
-  
+end
+end
+
 speak_to_grandma ("I LOVE YOU GRANDMA!")
 
 
